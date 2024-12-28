0d1523683e42ac745d75efeb0395dc08f59ea0b1
Bruno Martins <bgcngm@gmail.com>
2024-12-28 22:23:20 +0000

    gts7lwifi: Set up fingerprint sensor
    Change-Id: I49fb2071e416e8358601c5d2b90ece12efcf6388

--- device.mk
+++ device.mk
@@ -28,7 +28,8 @@ PRODUCT_PACKAGES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Sensors
 PRODUCT_COPY_FILES += \
