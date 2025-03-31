9f37d6e5056e787b6800225e02784de3ce0dc740
Bruno Martins <bgcngm@gmail.com>
2025-03-31 09:43:47 +0100

    gts7lwifi: Remove lib_fixups_user_type usage
    Change-Id: Ifdb575dcd082591406897a1163b224c861a6a4e8

--- extract-files.py
+++ extract-files.py
@@ -6,7 +6,6 @@
 
 from extract_utils.fixups_lib import (
     lib_fixups,
-    lib_fixups_user_type,
 )
 from extract_utils.main import (
     ExtractUtils,
@@ -17,11 +16,6 @@ namespace_imports = [
     'vendor/samsung/sm8250-common',
     'vendor/qcom/opensource/display',
 ]
-
-
-lib_fixups: lib_fixups_user_type = {
-    **lib_fixups,
-}
 
 module = ExtractUtilsModule(
     'gts7lwifi',
