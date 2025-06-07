c08fea0d58a4aa86d3051470517a4804a840b01f
Bruno Martins <bgcngm@gmail.com>
2025-06-08 00:46:23 +0100

    gts7lwifi: Opt-out for touchscreen gestures feature
    Currently, the custom touch HAL only marks "Single Tap" as available,
    which this target doesn't support.
    
    Change-Id: Ifd3b8ca346bbea27246be693cdd84a61ec4fe470

--- device.mk
+++ device.mk
@@ -42,5 +42,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Touch features
+$(call soong_config_set,samsungTouchVars,enableTouchscreenGestures,false)
+
 # Inherit from vendor blobs
 $(call inherit-product-if-exists, vendor/samsung/gts7lwifi/gts7lwifi-vendor.mk)
